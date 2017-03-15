3b33a9bb1e9eaaf8f3a4a58c80f64d8645b8fefd
Rafael Castro do UOL <quirinobytes@gmail.com>
2017-03-15 15:30:27 -0300

    Novos arquivos para cliente e server, funcionando OK.

---
+++ qmonitor-server/config/app_config.js
@@ -0,0 +1,49 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var conf = require("./config.js");
+var winston = require('winston');
+
+var logger = new (winston.Logger)({
+	transports: [
+	new (winston.transports.File) ({
+		name:'info-file',
+		filename: 'filelog-info.log',
+		level: 'info'}),
+	new (winston.transports.File)({
+		name: 'error-file',
+		filename: 'filelog-error.log',
+		level: 'error'})
+	]
+
+
+}); 
+
+
+
+logger.warn("A variavel de log contem");
+
+// #### VARIAVEIS GLOBAIS
+global.qmonitorserverip = 'a3-zico1' ;
+global.port = 8080;
+
+var app = module.exports = express();
+
+app.listen(port);
+
+//Configurar o uso do body-parser para receber URL encoded
+app.use(bodyParser.urlencoded({extended:true}));
+
+//Configurar o bodyparser para receber Json tmb.
+app.use(bodyParser.json());
+
+// Configurar outras aplicações que possam usar a API.
+app.use (function (req,res,next){
+// Permitindo acesso a todo mundo * / definir do cliente IP.
+res.setHeader ('Access-Control-Allow-Origin', '*');
+// Definir os metodos HTTP que poderão ser aceitos.
+res.setHeader ('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+ // Definir os cabeçalhos que serão aceitos.
+res.setHeader ('Access-Control-Allow-Headers', 'X-Requested-With', 'content-type','Authorization');
+next();
+})
+
---
+++ qmonitor-server/config/config.js
@@ -0,0 +1,6 @@
+
+
+//Arquivo de configuração do qmonitor
+
+
+global.logfile = "/var/log/qmaonitor.log";
