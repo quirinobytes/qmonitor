9e77f5fda9fca1c3513afb586cd5fd5620c6ad4d
Rafael Quirino <quirinobytes@gmail.com>
2017-03-27 01:05:48 -0300

    adding clean log board button, but not active.

--- qmonitor-server/config/app_config.js
+++ qmonitor-server/config/app_config.js
@@ -3,7 +3,7 @@ var bodyParser = require('body-parser');
 var conf = require("./config.js");
 var log4js = require('log4js');
 // #### VARIAVEIS GLOBAIS
-global.qmonitorserverip = '189.55.194.115' ;
+global.qmonitorserverip = '0.0.0.0' ;
 global.port = 8080;
 //setting timeout to 20 seconds
 timeout = 20000;
